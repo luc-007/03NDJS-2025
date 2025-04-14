de4f38fc1a0a8836bdd7398404a1d78b9da4b580
luc-007 <kemtaluc23@gmail.com>
2025-04-14 16:57:06 +0200

    modification du fichier main.js et maths.js

--- Day1/sandbox/First-Step/main.js
+++ Day1/sandbox/First-Step/main.js
@@ -1,2 +1,4 @@
 import { addition, soustraction, multiplication, division } from './maths.js';
-import readline from 'readline';
+
+let result = addition(2, 3);
+console.log(result);
--- Day1/sandbox/First-Step/maths.js
+++ Day1/sandbox/First-Step/maths.js
@@ -1,25 +1,25 @@
-function addition(a, b) {
+export function addition(a, b) {
   const resultat = a + b;
   console.log(`Addition: ${a} + ${b} = ${resultat}`);
   return resultat;
 }
 
-function soustraction(a, b) {
+export function soustraction(a, b) {
   const resultat = a - b;
   console.log(`Soustraction: ${a} - ${b} = ${resultat}`);
   return resultat;
 }
 
-function multiplication(a, b) {
+export function multiplication(a, b) {
   const resultat = a * b;
   console.log(`Multiplication: ${a} * ${b} = ${resultat}`);
   return resultat;
 }
 
-function division(a, b) {
+export function division(a, b) {
   if (b === 0) {
     console.log("Erreur : Division par zéro");
-    return "Erreur : Division par zéro"; 
+    return "Erreur : Division par zéro";
   }
   const resultat = a / b;
   console.log(`Division: ${a} / ${b} = ${resultat}`);
