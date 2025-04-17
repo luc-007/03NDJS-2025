108c6b500964348b5f544afa48099e81040ac731
luc-007 <kemtaluc23@gmail.com>
2025-04-17 15:01:46 +0200

    ajout des authentifications

--- Day-4/project-root/controllers/authController.js
+++ Day-4/project-root/controllers/authController.js
@@ -1,10 +1,33 @@
-//Importation des modules nécessaires
-const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const { users } = require('../models/user');
+const { users } = require('../models/userModel');
 
-//instription 
 const register = async(req, res) => {
-    try {} catch (error) {}
-};
+    try {
+        const { email, password } = req.body;
+
+        // Vérifie si l'utilisateur existe déjà
+        if (users.find(user => user.email === email)) {
+            return res.status(400).json({ message: 'L\'utilisateur existe déjà' });
+        }
+
+        // Hashage du mot de passe
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newUser = {
+            id: users.length + 1,
+            email,
+            password: hashedPassword,
+        };
+        users.push(newUser);
+
+        // Retourner l'utilisateur (sans le mot de passe)
+        const { password: excludedPassword, ...user } = newUser;
+        res.status(201).json(user);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Erreur serveur' });
+    }
+};  res.status(500).json({ message: 'Erreur serveur' });
+      }
+    };
