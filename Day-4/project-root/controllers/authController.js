7c6a105830e4aeb11dfbac84ae439c7cdd3c6253
luc-007 <kemtaluc23@gmail.com>
2025-04-17 13:13:44 +0200

    stockage en mémoire

---
+++ Day-4/project-root/controllers/authController.js
@@ -0,0 +1,10 @@
+//Importation des modules nécessaires
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { users } = require('../models/user');
+
+//instription 
+const register = async(req, res) => {
+    try {} catch (error) {}
+};
--- Day-4/project-root/models/userModel.js
+++ Day-4/project-root/models/userModel.js
@@ -2,5 +2,41 @@
 let users = [];
 
 module.exports = {
-    users,
+    async createUser(user) {
+        users.push(user);
+        return user;
+    },
+
+    async findByEmail(email) {
+        return users.find(user => user.email === email);
+    },
+
+    async findById(id) {
+        return users.find(user => user.id === id);
+    },
+
+    async findAll() {
+        return users;
+    },
+
+    async updateUser(id, updatedUser) {
+        const index = users.findIndex(user => user.id === id);
+        if (index !== -1) {
+            users[index] = {...users[index], ...updatedUser };
+            return users[index];
+        }
+        return null;
+    },
+
+    async deleteUser(id) {
+        const index = users.findIndex(user => user.id === id);
+        if (index !== -1) {
+            const deletedUser = users.splice(index, 1);
+            return deletedUser[0];
+        }
+        return null;
+    },
+
+    // Exportation de la liste des utilisateurs pour les tests
+    _users: users
 };
