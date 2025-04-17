543264974bb7b691c08fa8fd9a58563938b130f1
luc-007 <kemtaluc23@gmail.com>
2025-04-17 13:36:16 +0200

    importation des package, ajout d'un middleware et des routes

--- Day-4/project-root/server.js
+++ Day-4/project-root/server.js
@@ -1,3 +1,14 @@
-import express from "express";
-import express from ".db/connectDB.js";
-import autoRoutes from "./routes/auth.js";
+const express = require('express');
+const app = express();
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/users');
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use('/', authRoutes);
+app.use('/', userRoutes);
+
+const port = 3000;
+app.listen(port, () => console.log(`Serveur en écoute sur le port ${port}`));
